b36b6f0d318a04c9c21394de013a85d7ca25a187
HyunnSangCho <zx251874@naver.com>
2022-06-01 22:46:18 +0900

    Updates

---
+++ precache-manifest.233d13193fb62bcd116302ff82cd37ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bab67ca8ce46e41b68d4",
+    "url": "/react-movie-app/static/js/main.bab67ca8.chunk.js"
+  },
+  {
+    "revision": "2c4baf5a2379ae2c141e",
+    "url": "/react-movie-app/static/js/1.2c4baf5a.chunk.js"
+  },
+  {
+    "revision": "bdcfacafbc62971f5461",
+    "url": "/react-movie-app/static/js/runtime~main.bdcfacaf.js"
+  },
+  {
+    "revision": "4c18600e348f80e524f67c57f5f76c7e",
+    "url": "/react-movie-app/index.html"
+  }
+];
